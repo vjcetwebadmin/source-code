6f8291911bf2ee285d532e402a2c249d22a07ddb
erpadmin <webadmin@vjcet.org>
2022-04-29 10:33:40 +0530

    college timings

--- src/Pages/NewsContainers/Newsnine.js
+++ src/Pages/NewsContainers/Newsnine.js
@@ -40,11 +40,11 @@ class Newsnine extends React.Component {
 
     NewsTitle = {
         instanceID: "NewsTitle",
-        title: "Wanted Computer Programmer and Computer Lab Instructor"
+        title: "College Timing"
     };
     Newscontent = {
         instanceID: "Newscontent",
-        title: " The 1st batch of KTU(2015-19) secured 91.82% pass"
+        title: "College Timing on 29th and 30th (NAAC visiting days)"
     };
     
     NewsImagedata1 = {
@@ -72,10 +72,31 @@ class Newsnine extends React.Component {
                 <div className="contain">
                    
                    <HeadingOne data={this.NewsTitle} />
+                    <table id="tableformat" >
+                    <tbody>
+                        <tr><th colSpan="3">College Timing</th></tr>
+                            <tr><td><b>Time</b></td><td><b>Period</b></td><td><b>Duration</b></td></tr>
+                            <tr><td>8.55 to 9.50</td><td>1</td><td>55 Min</td></tr>
+                            <tr><td>9.50 to 10.45</td><td>2</td><td>55 Min</td></tr>
+                            <tr><td colSpan="3" style={{textAlign:"center"}}>Interval 10 Min</td></tr>
+                            <tr><td>10.55 to 11.50</td><td>3</td><td>55 Min</td></tr>
+                            <tr><td>11.50 to 12.45</td><td>4</td><td>55 Min</td></tr>
+                            <tr><td colSpan="3"  style={{textAlign:"center"}}>Noon Interval 55 Min</td></tr>
+                            <tr><td>1.40 to 2.30</td><td>5</td><td>50 Min</td></tr>
+                            <tr><td>2.30 to 3.20</td><td>6</td><td>50 Min</td></tr>
+                            <tr><td colSpan="3"  style={{textAlign:"center"}}>Interval 5 Min</td></tr>
+                            <tr><td>3.25 to 4.15</td><td>7</td><td>50 Min</td></tr>
+
+                            
+                    </tbody>
+                    </table>
+
+
                    
+                {/*
                  
                   <iframe src="https://vjcet.org/downloads/news/LABINSTRUCTOR.pdf" width="100%" zoom="80%" height="850" title="Campus Map" align="center"></iframe>
-                   
+                */}
                 </div>
                 <Footer data={this.HomePageFooter} />
                 {this.state.showMenu ? (<MenuOverlay closeMenuFunction={this.closeMenu} />) : ("")}
