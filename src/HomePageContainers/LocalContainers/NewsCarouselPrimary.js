30caf2270ef663967db4d6c038616c7d33f5a653
erpadmin <webadmin@vjcet.org>
2022-08-03 17:41:41 +0530

    new vjcet position size change

--- src/HomePageContainers/LocalContainers/NewsCarouselPrimary.js
+++ src/HomePageContainers/LocalContainers/NewsCarouselPrimary.js
@@ -129,13 +129,13 @@ class NewsCarouselPrimary extends Component {
     instanceID: "NewsPrimaryOneImage",
     image: "NewsSection/BigNews/4thkathelic2022.jpg",
     //Recommended Image Size is 733px x 400px.
-    width: "64%"
+    width: "75%"
   }; 
   Image20 = {
     instanceID: "NewsPrimaryOneImage",
     image: "NewsSection/BigNews/17thposition2022.jpg",
     //Recommended Image Size is 733px x 400px.
-    width: "64%"
+    width: "75%"
   }; 
 
   Title6 = {
