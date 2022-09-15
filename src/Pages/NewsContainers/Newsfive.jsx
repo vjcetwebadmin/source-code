26c22e668f2f8830c7aae39543553e1a9f0d2e31
erpadmin <webadmin@vjcet.org>
2022-09-15 16:32:17 +0530

    add notice and staff

--- src/Pages/NewsContainers/Newsfive.jsx
+++ src/Pages/NewsContainers/Newsfive.jsx
@@ -105,13 +105,14 @@ class Newsfive extends React.Component {
                  <br />
                  <h3 className="text_red"><b>Dear Parent, Please remit 4th semester tution fees of your ward, date extended to 30-06-2021 without fine and 12-07-2021 with fine.</b></h3>
                 <br /> */}
-                 <p></p><br></br>
-                 <p><b>Semester 2</b></p>
-                <iframe src="https://vjcet.org/downloads/news/S2Fee2022.jpg" width="100%" height="600" title="Campus Map" align="center"></iframe> 
-                <p><b>Semester 4</b></p>              
-                 <iframe src="https://vjcet.org/downloads/news/FeeS4.jpg" width="100%" height="600" title="Campus Map" align="center"></iframe>    
+                <p></p><br></br>
+                <p><b>Semester 3</b></p>
+                    <iframe src="https://vjcet.org/downloads/news/NoticeS3TF.jpg" width="100%" height="600" title="Campus Map" align="center"></iframe> 
+                {/*<p><b>Semester 4</b></p>              
+                    <iframe src="https://vjcet.org/downloads/news/FeeS4.jpg" width="100%" height="600" title="Campus Map" align="center"></iframe>    
                 <p><b>Semester 6</b></p>              
-                <iframe src="https://vjcet.org/downloads/news/Untitled-1.jpg" width="100%" height="600" title="Campus Map" align="center"></iframe>    
+                    <iframe src="https://vjcet.org/downloads/news/Untitled-1.jpg" width="100%" height="600" title="Campus Map" align="center"></iframe>    
+                */}
                 </div>
                 <Footer data={this.HomePageFooter} />
                 {this.state.showMenu ? (<MenuOverlay closeMenuFunction={this.closeMenu} />) : ("")}
