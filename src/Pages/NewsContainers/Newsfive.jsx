324f3a3a54e74d352c4b7cdfacde41bfcebf5839
erpadmin <webadmin@vjcet.org>
2022-11-16 13:12:43 +0530

    add BHM Faculty and bus fare notice

--- src/Pages/NewsContainers/Newsfive.jsx
+++ src/Pages/NewsContainers/Newsfive.jsx
@@ -106,6 +106,9 @@ class Newsfive extends React.Component {
                  <h3 className="text_red"><b>Dear Parent, Please remit 4th semester tution fees of your ward, date extended to 30-06-2021 without fine and 12-07-2021 with fine.</b></h3>
                 <br /> */}
                 <p></p><br></br>
+                
+                <p><b>College Bus Fare</b></p>              
+                    <iframe src="https://vjcet.org/downloads/news/Bus_notice.pdf" width="100%" height="600" title="Campus Map" align="center"></iframe>  
                 <p><b>Caution Deposit</b></p>              
                     <iframe src="https://vjcet.org/downloads/news/NoticeCautionDeposit.jpg" width="100%" height="600" title="Campus Map" align="center"></iframe>  
                 <p><b>PFA</b></p>    
